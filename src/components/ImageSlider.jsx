8a5a3e7049b51aaa612d2f20f954e3dabd0930b8
benyou10 <ybenzian016@gmail.com>
2024-09-04 11:11:55 +0100

    fix3

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ImageSlider = (props) => {
     const [currentFrame, setCurrentFrame] = useState(0);
